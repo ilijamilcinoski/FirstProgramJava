18dd545522ed3f9cb04153c1cf87d7e364bcf527
Ilija <milcinoskiilija@gmail.com>
2015-02-15 23:11:33 +0100

    This is my first app written in Java language You need to have Netbeans to see the code

---
+++ src/Week1/HelloWorld.java
@@ -0,0 +1,14 @@
+package Week1;
+
+/**
+ *Shows system messages 
+ * @author im23405
+ * @author Ilija Milcinoski
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Zdravo,Hello!");
+        System.out.println("Congratiulation you have done it!");
+    }
+}
---
+++ src/Week1/I.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Week1;
+
+/**
+ *
+ * @author im23405
+ */
+public class I {
+    
+    
+public static void main (String[] args) {
+
+    System.out.println("Hello and Welcome User!");
+    System.err.println("This is a system message");
+    
+ 
+}
+}
