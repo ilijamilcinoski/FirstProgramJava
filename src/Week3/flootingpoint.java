402c2adf1cc8a46af2561ee5bfab6a180a0792d9
Ilija <milcinoskiilija@gmail.com>
2015-02-25 22:07:24 +0100

    New java examples for week 3

---
+++ src/Week3/flootingpoint.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Week3;
+
+/**
+ *
+ * @author Ilija Milchinoski 
+ */
+public class flootingpoint {
+public static void main(String[] args) {
+  double value1=(12.5 + 9 / 2);
+  double value2= (4.8 / 4 + 9 % 2);
+  double value3= (14.4 % 4 + 14 / 4);
+  double value4= (15.0 / 3-16%3); 
+  double value5= (15/3-16%3);
+  System.out.println(value1);
+  System.out.println(value2);
+  System.out.println(value3);
+  System.out.println(value4);
+  System.out.println(value5);
+  
+}
+    
+}
