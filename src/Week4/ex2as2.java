7b742d27090040d1e03252f168642086f2c26412
ilijamilcinoski <milcinoskiilija@gmail.comgit config --global user.name Ilija>
2015-03-09 23:41:04 +0100

    Additional Examples for week 4

---
+++ src/Week4/ex2as2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Week4;
+
+/**
+ *
+ * @author im23405
+ */
+public class ex2as2 {
+ public static void main (String[] args) {
+int i = 12;
+int j = 5;
+i = i % j;
+System.out.println("The Result is"+i+j+i);
+
+ } 
+}
